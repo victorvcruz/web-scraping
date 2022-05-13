6808153ac469a17f6b588a9b00bacd3933cc8bc3
victorvcruz <hugov4308@gmail.com>
2022-05-13 12:41:29 -0300

    feat: add filters and image_id

--- read_pages.py
+++ read_pages.py
@@ -6,25 +6,22 @@ from database import insert
 from product import Product
 
 
-all_states = ['ac', 'es', 'pb', 'ro', 'al', 'go', 'pr', 'rr', 'ap', 'ma', 'pe', 'sc', 'am', 'mt', 'pi',
-              'sp', 'ba', 'ms', 'rj', 'se', 'ce', 'mg', 'rn', 'to', 'pa', 'rs']
+def create_url(search, state, page, price_min, price_max, filter):
+    return 'https://{}.olx.com.br/?o={}&pe={}&ps={}&q={}{}'.format(state, page, price_max, price_min, search, filter)
 
 
-def create_url(search, state, page):
-    return 'https://{}.olx.com.br/?o={}&q={}'.format(state, page, search)
-
-
-def read_pages(search, page_start, page_end, states):
+def read_pages(search, page_start, page_end, states, price_min, price_max, filter):
 
     for page in range(page_start, page_end+1, +1):
 
         for state in states:
 
-            url = Request(create_url(search, state, page), headers={'User-Agent': 'Mozilla/5.0'})
+            url = Request(create_url(search, state, page, price_min, price_max, filter), headers={'User-Agent': 'Mozilla/5.0'})
             webpage = urlopen(url).read()
             soup = BeautifulSoup(webpage, 'html.parser')
             div_products = soup.find_all(config.div_type, class_=config.div_class)
 
             for div_product in div_products:
                 prd = Product(div_product, state)
-                print(prd.state)
+                if prd.image != config.image_not_found:
+                    print(prd.price)
